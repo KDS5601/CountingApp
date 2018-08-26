96c8444b74046f32f6eedfa4341644c6e64ff501
대성 김 <DaeSeong@jumong.kr>
2018-08-26 19:31:20 +0900

    뼈대 완성

--- CountingApp/CountingApp/MainPage.xaml.cs
+++ CountingApp/CountingApp/MainPage.xaml.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace CountingApp
 {
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
-            InitializeComponent();
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+
+            int countNum = 0;
+
+            CountUp.Clicked += (sender, e) => { ++countNum; CountingLabel.Text = countNum.ToString(); };
+            CountDown.Clicked += (sender, e) => { --countNum; CountingLabel.Text = countNum.ToString(); };
+            Reset.Clicked += (sender, e) => { countNum = 0; CountingLabel.Text = countNum.ToString(); };
         }
     }
 }
